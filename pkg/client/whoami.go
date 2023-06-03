c72ab238ac506f246c5289722841a77e496deddf
Paul Querna <pquerna@apache.org>
2023-06-03 22:51:57 +0000

    Have whoami echo back your user details, not your princple id

--- pkg/client/whoami.go
+++ pkg/client/whoami.go
@@ -6,7 +6,7 @@ import (
 	"github.com/conductorone/cone/internal/c1api"
 )
 
-func (c *client) WhoAmI(ctx context.Context) (*c1api.C1ApiAuthV1IntrospectResponse, error) {
+func (c *client) AuthIntrospect(ctx context.Context) (*c1api.C1ApiAuthV1IntrospectResponse, error) {
 	introspectResp, resp, err := c.apiClient.DefaultAPI.C1ApiAuthV1AuthIntrospect(ctx).Execute()
 	if err != nil {
 		return nil, err

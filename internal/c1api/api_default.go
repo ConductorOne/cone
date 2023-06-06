5732a9dd7787c6701ec247514b621b91ccb8ecfa
Logan Saso <logansaso@gmail.com>
2023-06-06 18:18:02 +0000

    Build api

--- internal/c1api/api_default.go
+++ internal/c1api/api_default.go
@@ -23,6 +23,117 @@ import (
 // DefaultAPIService DefaultAPI service
 type DefaultAPIService service
 
+type DefaultAPIC1ApiAppV1AppEntitlementUserBindingServiceGetRequest struct {
+	ctx context.Context
+	ApiService *DefaultAPIService
+	appId string
+	appEntitlementId string
+	appUserId string
+}
+
+func (r DefaultAPIC1ApiAppV1AppEntitlementUserBindingServiceGetRequest) Execute() (*C1ApiAppV1GetAppEntitlementUserBindingResponse, *http.Response, error) {
+	return r.ApiService.C1ApiAppV1AppEntitlementUserBindingServiceGetExecute(r)
+}
+
+/*
+C1ApiAppV1AppEntitlementUserBindingServiceGet Method for C1ApiAppV1AppEntitlementUserBindingServiceGet
+
+Invokes the c1.api.app.v1.AppEntitlementUserBindingService.Get method.
+
+ @param ctx context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
+ @param appId
+ @param appEntitlementId
+ @param appUserId
+ @return DefaultAPIC1ApiAppV1AppEntitlementUserBindingServiceGetRequest
+*/
+func (a *DefaultAPIService) C1ApiAppV1AppEntitlementUserBindingServiceGet(ctx context.Context, appId string, appEntitlementId string, appUserId string) DefaultAPIC1ApiAppV1AppEntitlementUserBindingServiceGetRequest {
+	return DefaultAPIC1ApiAppV1AppEntitlementUserBindingServiceGetRequest{
+		ApiService: a,
+		ctx: ctx,
+		appId: appId,
+		appEntitlementId: appEntitlementId,
+		appUserId: appUserId,
+	}
+}
+
+// Execute executes the request
+//  @return C1ApiAppV1GetAppEntitlementUserBindingResponse
+func (a *DefaultAPIService) C1ApiAppV1AppEntitlementUserBindingServiceGetExecute(r DefaultAPIC1ApiAppV1AppEntitlementUserBindingServiceGetRequest) (*C1ApiAppV1GetAppEntitlementUserBindingResponse, *http.Response, error) {
+	var (
+		localVarHTTPMethod   = http.MethodGet
+		localVarPostBody     interface{}
+		formFiles            []formFile
+		localVarReturnValue  *C1ApiAppV1GetAppEntitlementUserBindingResponse
+	)
+
+	localBasePath, err := a.client.cfg.ServerURLWithContext(r.ctx, "DefaultAPIService.C1ApiAppV1AppEntitlementUserBindingServiceGet")
+	if err != nil {
+		return localVarReturnValue, nil, &GenericOpenAPIError{error: err.Error()}
+	}
+
+	localVarPath := localBasePath + "/api/v1/grants/app/{app_id}/entitlement/{app_entitlement_id}/user/{app_user_id}"
+	localVarPath = strings.Replace(localVarPath, "{"+"app_id"+"}", url.PathEscape(parameterValueToString(r.appId, "appId")), -1)
+	localVarPath = strings.Replace(localVarPath, "{"+"app_entitlement_id"+"}", url.PathEscape(parameterValueToString(r.appEntitlementId, "appEntitlementId")), -1)
+	localVarPath = strings.Replace(localVarPath, "{"+"app_user_id"+"}", url.PathEscape(parameterValueToString(r.appUserId, "appUserId")), -1)
+
+	localVarHeaderParams := make(map[string]string)
+	localVarQueryParams := url.Values{}
+	localVarFormParams := url.Values{}
+
+	// to determine the Content-Type header
+	localVarHTTPContentTypes := []string{}
+
+	// set Content-Type header
+	localVarHTTPContentType := selectHeaderContentType(localVarHTTPContentTypes)
+	if localVarHTTPContentType != "" {
+		localVarHeaderParams["Content-Type"] = localVarHTTPContentType
+	}
+
+	// to determine the Accept header
+	localVarHTTPHeaderAccepts := []string{"application/json"}
+
+	// set Accept header
+	localVarHTTPHeaderAccept := selectHeaderAccept(localVarHTTPHeaderAccepts)
+	if localVarHTTPHeaderAccept != "" {
+		localVarHeaderParams["Accept"] = localVarHTTPHeaderAccept
+	}
+	req, err := a.client.prepareRequest(r.ctx, localVarPath, localVarHTTPMethod, localVarPostBody, localVarHeaderParams, localVarQueryParams, localVarFormParams, formFiles)
+	if err != nil {
+		return localVarReturnValue, nil, err
+	}
+
+	localVarHTTPResponse, err := a.client.callAPI(req)
+	if err != nil || localVarHTTPResponse == nil {
+		return localVarReturnValue, localVarHTTPResponse, err
+	}
+
+	localVarBody, err := io.ReadAll(localVarHTTPResponse.Body)
+	localVarHTTPResponse.Body.Close()
+	localVarHTTPResponse.Body = io.NopCloser(bytes.NewBuffer(localVarBody))
+	if err != nil {
+		return localVarReturnValue, localVarHTTPResponse, err
+	}
+
+	if localVarHTTPResponse.StatusCode >= 300 {
+		newErr := &GenericOpenAPIError{
+			body:  localVarBody,
+			error: localVarHTTPResponse.Status,
+		}
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	err = a.client.decode(&localVarReturnValue, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+	if err != nil {
+		newErr := &GenericOpenAPIError{
+			body:  localVarBody,
+			error: err.Error(),
+		}
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	return localVarReturnValue, localVarHTTPResponse, nil
+}
+
 type DefaultAPIC1ApiAppV1AppResourceServiceGetRequest struct {
 	ctx context.Context
 	ApiService *DefaultAPIService

5aa50c9695a524ea92500ebd576d425258ebf724
Logan Saso <logansaso@gmail.com>
2023-06-07 17:24:02 -0700

    Formatted imports, support more time formats

--- cmd/cone/task_search.go
+++ cmd/cone/task_search.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"errors"
+	"strings"
+	"time"
+
 	"github.com/conductorone/cone/internal/c1api"
 	"github.com/conductorone/cone/pkg/output"
 	"github.com/spf13/cobra"
-	"strings"
-	"time"
 )
 
 func searchTasksCmd() *cobra.Command {
@@ -32,6 +34,20 @@ func searchTasksCmd() *cobra.Command {
 	return cmd
 }
 
+var validTimeFormats = []string{
+	time.RFC3339,
+	time.Layout,
+	time.RFC1123,
+	time.RFC1123Z,
+	time.Stamp,
+	time.DateTime,
+	time.DateOnly,
+	time.TimeOnly,
+	time.Kitchen,
+}
+
+var noValidTimeFound = errors.New("could not parse time, valid formats are go's RFC3339, Layout, RFC1123, RFC1123Z, Stamp, DateTime, DateOnly, TimeOnly, and Kitchen")
+
 func searchTasksRun(cmd *cobra.Command, args []string) error {
 	ctx, c, v, err := cmdContext(cmd)
 	if err != nil {
@@ -40,20 +56,30 @@ func searchTasksRun(cmd *cobra.Command, args []string) error {
 
 	var createdAfter *time.Time
 	if v.GetString(createdAfterFlag) != "" {
-		createdAfterParsed, err := time.Parse(time.RFC3339, v.GetString(createdAfterFlag))
-		if err != nil {
-			return err
+		for _, validTimeFormat := range validTimeFormats {
+			createdAfterParsed, err := time.Parse(validTimeFormat, v.GetString(createdAfterFlag))
+			if err != nil {
+				continue
+			}
+			createdAfter = &createdAfterParsed
 		}
-		createdAfter = &createdAfterParsed
+		if createdAfter == nil {
+			return noValidTimeFound
+		}
 	}
 
 	var createdBefore *time.Time
 	if v.GetString(createdBeforeFlag) != "" {
-		createdBeforeParsed, err := time.Parse(time.RFC3339, v.GetString(createdBeforeFlag))
-		if err != nil {
-			return err
+		for _, validTimeFormat := range validTimeFormats {
+			createdBeforeParsed, err := time.Parse(validTimeFormat, v.GetString(createdBeforeFlag))
+			if err != nil {
+				continue
+			}
+			createdBefore = &createdBeforeParsed
 		}
-		createdBefore = &createdBeforeParsed
+		if createdAfter == nil {
+			return noValidTimeFound
+		}
 	}
 
 	var includeDeleted *bool

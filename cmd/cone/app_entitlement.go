f21dde616f8b4653ad6e43e3a60c5bfa5f7d448e
Grant Kopczenski <grant.kopczenski@conductorone.com>
2023-06-29 20:31:11 +0000

    Added the flag to work with drop cmd as well

--- cmd/cone/app_entitlement.go
+++ cmd/cone/app_entitlement.go
@@ -10,7 +10,6 @@ type Entitlement shared.AppEntitlement
 func (r *Entitlement) Header() []string {
 	return []string{
 		"Entitlement",
-		"Alias",
 		"Description",
 	}
 }
@@ -19,6 +18,5 @@ func (r *Entitlement) Rows() [][]string {
 	return [][]string{{
 		client.StringFromPtr(r.DisplayName),
 		client.StringFromPtr(r.Description),
-		client.StringFromPtr(r.Alias),
 	}}
 }

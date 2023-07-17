088c1d6b2e53cb644cffef3927b5bac4861bd978
Grant Kopczenski <grant.kopczenski@conductorone.com>
2023-07-17 21:25:23 +0000

    Lint fix

--- cmd/cone/token.go
+++ cmd/cone/token.go
@@ -58,6 +58,7 @@ func tokenRun(cmd *cobra.Command, args []string) error {
 	}
 
 	if v.GetBool(rawTokenFlag) {
+		//nolint:forbidigo // We want to raw-print the bearer if this flag is included
 		fmt.Println(tokenObj.AccessToken)
 		return nil
 	}

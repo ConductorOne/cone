a6291b2aae5d273928f82ecc41d676f0e7e4ee91
Grant Kopczenski <grant.kopczenski@conductorone.com>
2023-07-17 21:20:32 +0000

    Added output manager and --raw flag

--- cmd/cone/token.go
+++ cmd/cone/token.go
@@ -4,9 +4,16 @@ import (
 	"fmt"
 
 	"github.com/conductorone/cone/pkg/client"
+	"github.com/conductorone/cone/pkg/output"
 
 	"github.com/spf13/cobra"
 )
+
+type Token struct {
+	AccessToken string `header:"access_token"`
+	TokenType   string `header:"token_type"`
+	Expiry      string `header:"expiry"`
+}
 
 func tokenCmd() *cobra.Command {
 	cmd := &cobra.Command{
@@ -14,7 +21,7 @@ func tokenCmd() *cobra.Command {
 		Short: "",
 		RunE:  tokenRun,
 	}
-
+	addRawTokenFlag(cmd)
 	return cmd
 }
 
@@ -44,7 +51,42 @@ func tokenRun(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	print(token)
+	tokenObj := Token{
+		AccessToken: token.AccessToken,
+		TokenType:   token.TokenType,
+		Expiry:      token.Expiry.String(),
+	}
+
+	if v.GetBool(rawTokenFlag) {
+		fmt.Println(tokenObj.AccessToken)
+		return nil
+	}
+
+	outputManager := output.NewManager(ctx, v)
+	err = outputManager.Output(ctx, &tokenObj, output.WithTransposeTable())
+	if err != nil {
+		return err
+	}
 
 	return nil
+}
+
+func (r *Token) Header() []string {
+	return []string{
+		"Access Token",
+		"Token Type",
+		"Expiry",
+	}
+}
+
+func (r *Token) rows() []string {
+	return []string{
+		r.AccessToken,
+		r.TokenType,
+		r.Expiry,
+	}
+}
+
+func (r *Token) Rows() [][]string {
+	return [][]string{r.rows()}
 }

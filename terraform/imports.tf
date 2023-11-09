24bf9a759b9044ccd9fd3cb7c9629cfb8a7c5d11
Anthony Zhao <anthony.zhao@conductorone.com>
2023-11-09 12:37:01 -0800

    Working

---
+++ terraform/imports.tf
@@ -0,0 +1,91 @@
+    resource "conductorone_app" "id_2Qo1YQ24RQMUISlqtpJ4IPzxQdp" {
+        certify_policy_id = "20n6suDXRPLSiTnRLS8CURTCIq7"
+        description       = "Integrates with Okta Users, Groups and Roles"
+        display_name      = "Okta"
+        grant_policy_id   = "2XDtdd6VlXLm1P7gFVM5vwFMq7X"
+        monthly_cost_usd  = 11
+        revoke_policy_id  = "1xJnEz4VE1uT1xwHcFfMja0KF6Y"
+    }
+    resource "conductorone_app" "id_2Qo2VH99Q1qnqog9Vpk0KMrfn3o" {
+        display_name     = "ConductorOne"
+        grant_policy_id  = "1vPv2JswIqSI9M2DXN0eQhboegU"
+        monthly_cost_usd = 0
+        revoke_policy_id = "1xJnEz4VE1uT1xwHcFfMja0KF6Y"
+    }
+    resource "conductorone_app" "id_2Qo2VJ5Fccc8mgfA1DAtLujP4Ky" {
+        display_name     = "Okta Dashboard"
+        grant_policy_id  = "1vPv2JswIqSI9M2DXN0eQhboegU"
+        monthly_cost_usd = 0
+        revoke_policy_id = "1xJnEz4VE1uT1xwHcFfMja0KF6Y"
+    }
+    resource "conductorone_app" "id_2Qo2VKPZCCCkiSr9Af8DH5Jl4oA" {
+        display_name     = "Okta Browser Plugin"
+        grant_policy_id  = "1vPv2JswIqSI9M2DXN0eQhboegU"
+        monthly_cost_usd = 0
+        revoke_policy_id = "1xJnEz4VE1uT1xwHcFfMja0KF6Y"
+    }
+    resource "conductorone_app" "id_2Qo2VKyGbzEp6DUWoVltnau6clx" {
+        display_name     = "Okta Admin Console"
+        grant_policy_id  = "1vPv2JswIqSI9M2DXN0eQhboegU"
+        monthly_cost_usd = 0
+        revoke_policy_id = "1xJnEz4VE1uT1xwHcFfMja0KF6Y"
+    }
+    resource "conductorone_app" "id_2Qo36p0ZqiZMeEXY8FMDUwrca2H" {
+        display_name     = "Salesforce.com"
+        grant_policy_id  = "1vPv2JswIqSI9M2DXN0eQhboegU"
+        monthly_cost_usd = 0
+        revoke_policy_id = "1xJnEz4VE1uT1xwHcFfMja0KF6Y"
+    }
+    resource "conductorone_app" "id_2TisuhnWkxX6TzXnQP7YWs4Hqhj" {
+        display_name     = "2020 Spaces"
+        grant_policy_id  = "1vPv2JswIqSI9M2DXN0eQhboegU"
+        monthly_cost_usd = 0
+        revoke_policy_id = "1xJnEz4VE1uT1xwHcFfMja0KF6Y"
+    }
+    resource "conductorone_app" "id_2UOvT4aMAP7WetxdoU7gdvtpnG7" {
+        description      = "Integrates with Confluence"
+        display_name     = "Confluence"
+        grant_policy_id  = "1vPv2JswIqSI9M2DXN0eQhboegU"
+        monthly_cost_usd = 0
+        revoke_policy_id = "1xJnEz4VE1uT1xwHcFfMja0KF6Y"
+    }
+    resource "conductorone_app" "id_2VGMcAR9jhzz4J3RifiK7Za7p7b" {
+        description      = "Integrates with Expensify users, groups, roles, and apps"
+        display_name     = "Expensify"
+        grant_policy_id  = "1vPv2JswIqSI9M2DXN0eQhboegU"
+        monthly_cost_usd = 0
+        revoke_policy_id = "1xJnEz4VE1uT1xwHcFfMja0KF6Y"
+    }
+    resource "conductorone_app" "id_2Va7nPy3gJ4YJw7kKOyZbSA9xgM" {
+        description      = "a"
+        display_name     = "LDAP"
+        grant_policy_id  = "1vPv2JswIqSI9M2DXN0eQhboegU"
+        monthly_cost_usd = 0
+        revoke_policy_id = "1xJnEz4VE1uT1xwHcFfMja0KF6Y"
+    }
+    resource "conductorone_app" "id_2Va8BlQpp7RzEBscOtRO9iwhZ0h" {
+        description      = "Integrates with self-hosted Baton connectors."
+        display_name     = "Baton"
+        grant_policy_id  = "1vPv2JswIqSI9M2DXN0eQhboegU"
+        monthly_cost_usd = 0
+        revoke_policy_id = "1xJnEz4VE1uT1xwHcFfMja0KF6Y"
+    }
+    resource "conductorone_app" "id_2XBCJbM3t3F9NVI1q1zWD7dJz8R" {
+        display_name     = "Population Report Test"
+        grant_policy_id  = "1vPv2JswIqSI9M2DXN0eQhboegU"
+        monthly_cost_usd = 0
+        revoke_policy_id = "1xJnEz4VE1uT1xwHcFfMja0KF6Y"
+    }
+    resource "conductorone_app" "id_2XBCKLfhzH2sl1oVMpzlzlPQ3x8" {
+        display_name     = "Population Report Test"
+        grant_policy_id  = "1vPv2JswIqSI9M2DXN0eQhboegU"
+        monthly_cost_usd = 0
+        revoke_policy_id = "1xJnEz4VE1uT1xwHcFfMja0KF6Y"
+    }
+    resource "conductorone_app" "id_2XDrh4Zmim21ecKV7yEgM8JJmE7" {
+        description      = "Integrates with Twingate"
+        display_name     = "Twingate"
+        grant_policy_id  = "2XDtsEeogS7LLBfrFjEk8gSGIQO"
+        monthly_cost_usd = 0
+        revoke_policy_id = "2XDvAkgmJ3HpwIH7m85ykYwqXCq"
+    }

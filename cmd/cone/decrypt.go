e86df64bb033542e3d2d722aa91aeeec94aa8fa1
Brad Morgan <brad@morgabra.com>
2024-02-01 15:22:46 -0800

    stash

--- cmd/cone/decrypt.go
+++ cmd/cone/decrypt.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
 
+	v2 "github.com/conductorone/baton-sdk/pb/c1/connector/v2"
+	"github.com/conductorone/baton-sdk/pkg/crypto/providers"
+	"github.com/conductorone/baton-sdk/pkg/crypto/providers/jwk"
 	"github.com/spf13/cobra"
+	"google.golang.org/protobuf/encoding/protojson"
 
-	"github.com/conductorone/baton-sdk/pkg/crypto"
+	"github.com/conductorone/cone/pkg/client"
 )
 
 func decryptCredentialCmd() *cobra.Command {
@@ -19,7 +24,7 @@ func decryptCredentialCmd() *cobra.Command {
 }
 
 func decryptCredentialRun(cmd *cobra.Command, args []string) error {
-	_, _, _, err := cmdContext(cmd)
+	ctx, _, v, err := cmdContext(cmd)
 	if err != nil {
 		return err
 	}
@@ -27,10 +32,53 @@ func decryptCredentialRun(cmd *cobra.Command, args []string) error {
 	if err := validateArgLenth(1, args, cmd); err != nil {
 		return err
 	}
-	credential := args[0]
 
-	fmt.Println(credential)
+	// so, we store the ciphertext as []byte, but json serialization of this will be base64 encoded, so let's just require that as our input.
+	credentialDec, err := base64.StdEncoding.DecodeString(args[0])
+	if err != nil {
+		return fmt.Errorf("failed to base64 decode credential: %w", err)
+	}
 
-	crypto.EncryptionManager{}
+	provider, err := providers.GetEncryptionProvider(jwk.EncryptionProviderJwk)
+	if err != nil {
+		return fmt.Errorf("failed to get encryption provider: %w", err)
+	}
+
+	// FIXME(morgabra): This is a hack for testing.
+	ciphertext := &v2.EncryptedData{
+		Provider:       jwk.EncryptionProviderJwk,
+		EncryptedBytes: credentialDec,
+	}
+
+	// Get our secret key and dig out the private jwk, this is silly.
+	_, clientSecret, err := getCredentials(v)
+	if err != nil {
+		return fmt.Errorf("failed to get credentials: %w", err)
+	}
+	privateJWK, err := client.ParseSecret([]byte(clientSecret))
+	if err != nil {
+		return fmt.Errorf("failed to parse secret: %w", err)
+	}
+	privateKeyBytes, err := privateJWK.MarshalJSON()
+	if err != nil {
+		return fmt.Errorf("failed to marshal private jwk: %w", err)
+	}
+
+	plaintext, err := provider.Decrypt(ctx, ciphertext, privateKeyBytes)
+	if err != nil {
+		return fmt.Errorf("failed to decrypt credential: %w", err)
+	}
+
+	opt := &protojson.MarshalOptions{
+		Multiline:       true,
+		Indent:          "  ",
+		EmitUnpopulated: true,
+	}
+	pt, err := opt.Marshal(plaintext)
+	if err != nil {
+		return fmt.Errorf("failed to marshal plaintext: %w", err)
+	}
+	fmt.Printf("Decrypted credential: %s\n", pt)
+	fmt.Printf("Decrypted bytes: %s\n", plaintext.Bytes)
 	return nil
 }

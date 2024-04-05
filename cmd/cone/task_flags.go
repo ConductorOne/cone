73bb8259b93cc878f2c41f73de6456bbe7e8a814
Anthony Zhao <anthony.zhao@conductorone.com>
2024-04-05 14:11:43 -0700

    Lint

--- cmd/cone/task_flags.go
+++ cmd/cone/task_flags.go
@@ -3,14 +3,14 @@ package main
 import "github.com/spf13/cobra"
 
 const (
-	accessReviewIdsFlag    = "access-review-id"
-	appEntitlementIdsFlag  = "app-entitlement-id"
-	appResourceIdsFlag     = "app-resource-id"
-	appResourceTypeIdsFlag = "app-resource-type-id"
-	appUserSubjectIdsFlag  = "app-user-subject-id"
-	userSubjectIdsFlag     = "user-subject-id"
-	appIdsFlag             = "app-id"
-	assigneeIdsFlag        = "assignee-id"
+	accessReviewIDsFlag    = "access-review-id"
+	appEntitlementIDsFlag  = "app-entitlement-id"
+	appResourceIDsFlag     = "app-resource-id"
+	appResourceTypeIDsFlag = "app-resource-type-id"
+	appUserSubjectIDsFlag  = "app-user-subject-id"
+	userSubjectIDsFlag     = "user-subject-id"
+	appIDsFlag             = "app-id"
+	assigneeIDsFlag        = "assignee-id"
 	stateFlag              = "state"
 	taskTypeFlag           = "task-type"
 	includeDeletedFlag     = "include-deleted"
@@ -20,29 +20,29 @@ const (
 func addCommentFlag(cmd *cobra.Command) {
 	cmd.Flags().String(commentFlag, "", "Comment to add to the task when performing an action")
 }
-func addAccessReviewIdsFlag(cmd *cobra.Command) {
-	cmd.Flags().StringSlice(accessReviewIdsFlag, nil, "Filter tasks by access review id(s) (access review campaign this task belongs to)")
+func addAccessReviewIDsFlag(cmd *cobra.Command) {
+	cmd.Flags().StringSlice(accessReviewIDsFlag, nil, "Filter tasks by access review id(s) (access review campaign this task belongs to)")
 }
-func addAppEntitlementIdsFlag(cmd *cobra.Command) {
-	cmd.Flags().StringSlice(appEntitlementIdsFlag, nil, "Filter tasks by app entitlement id(s) (target app entitlement of the ticket)")
+func addAppEntitlementIDsFlag(cmd *cobra.Command) {
+	cmd.Flags().StringSlice(appEntitlementIDsFlag, nil, "Filter tasks by app entitlement id(s) (target app entitlement of the ticket)")
 }
-func addAppResourceIdsFlag(cmd *cobra.Command) {
-	cmd.Flags().StringSlice(appResourceIdsFlag, nil, "Filter tasks by app resource id(s) (target resource of the ticket)")
+func addAppResourceIDsFlag(cmd *cobra.Command) {
+	cmd.Flags().StringSlice(appResourceIDsFlag, nil, "Filter tasks by app resource id(s) (target resource of the ticket)")
 }
-func addAppResourceTypeIdsFlag(cmd *cobra.Command) {
-	cmd.Flags().StringSlice(appResourceTypeIdsFlag, nil, "Filter tasks by app resource type id(s) (target resource type of the task)")
+func addAppResourceTypeIDsFlag(cmd *cobra.Command) {
+	cmd.Flags().StringSlice(appResourceTypeIDsFlag, nil, "Filter tasks by app resource type id(s) (target resource type of the task)")
 }
-func addAppUserSubjectIdsFlag(cmd *cobra.Command) {
-	cmd.Flags().StringSlice(appUserSubjectIdsFlag, nil, "Filter tasks by app user subject id(s) (target of the task)")
+func addAppUserSubjectIDsFlag(cmd *cobra.Command) {
+	cmd.Flags().StringSlice(appUserSubjectIDsFlag, nil, "Filter tasks by app user subject id(s) (target of the task)")
 }
-func addUserSubjectIdsFlag(cmd *cobra.Command) {
-	cmd.Flags().StringSlice(userSubjectIdsFlag, nil, "Filter tasks by user subject id(s) (c1 user target of the task)")
+func addUserSubjectIDsFlag(cmd *cobra.Command) {
+	cmd.Flags().StringSlice(userSubjectIDsFlag, nil, "Filter tasks by user subject id(s) (c1 user target of the task)")
 }
-func addAppApplicationIdsFlag(cmd *cobra.Command) {
-	cmd.Flags().StringSlice(appIdsFlag, nil, "Filter tasks by app application id(s) (target application of the task)")
+func addAppApplicationIDsFlag(cmd *cobra.Command) {
+	cmd.Flags().StringSlice(appIDsFlag, nil, "Filter tasks by app application id(s) (target application of the task)")
 }
-func addAssigneesIds(cmd *cobra.Command) {
-	cmd.Flags().StringSlice(assigneeIdsFlag, nil, "Filter tasks by who is currently assigned to them")
+func addAssigneesIDs(cmd *cobra.Command) {
+	cmd.Flags().StringSlice(assigneeIDsFlag, nil, "Filter tasks by who is currently assigned to them")
 }
 
 func addQueryTaskFlag(cmd *cobra.Command) {

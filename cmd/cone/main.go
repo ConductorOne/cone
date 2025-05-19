8c7daacdf571981f58dc714a9b086846160e940c
Ali <ali@afalahi.com>
2025-05-18 18:56:21 -0600

    Refactor CLI command initialization in Cone
    This commit simplifies the command initialization in the Cone CLI by consolidating the command setup into a single function, `rootCmd()`. It retains the versioning and context management while removing redundant persistent flags and command additions, streamlining the code for better maintainability.

--- cmd/cone/main.go
+++ cmd/cone/main.go
@@ -36,43 +36,18 @@ func main() {
 }
 
 func runCli(ctx context.Context) int {
-	cliCmd := &cobra.Command{
-		Use:     "cone",
-		Short:   "Cone interacts with the ConductorOne API to manage access to entitlements.",
-		Version: version,
-		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
-			cmd.SetContext(ctx)
-			return nil
-		},
-		SilenceUsage:  true,
-		SilenceErrors: true,
+	cliCmd := rootCmd()
+	cliCmd.Version = version
+	cliCmd.PersistentPreRunE = func(cmd *cobra.Command, args []string) error {
+		cmd.SetContext(ctx)
+		return nil
 	}
-
-	cliCmd.PersistentFlags().StringP("profile", "p", "default", "The config profile to use.")
-	cliCmd.PersistentFlags().BoolP("non-interactive", "i", false, "Disable prompts.")
-	cliCmd.PersistentFlags().String("client-id", "", "Client ID")
-	cliCmd.PersistentFlags().String("client-secret", "", "Client secret")
-	cliCmd.PersistentFlags().String("api-endpoint", "", "Override the API endpoint")
-	cliCmd.PersistentFlags().StringP("output", "o", "table", "Output format. Valid values: table, json, json-pretty, wide.")
-	cliCmd.PersistentFlags().Bool("debug", false, "Enable debug logging")
 
 	err := initConfig(cliCmd)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
 		return 1
 	}
-
-	cliCmd.AddCommand(getCmd())
-	cliCmd.AddCommand(dropCmd())
-	cliCmd.AddCommand(whoAmICmd())
-	cliCmd.AddCommand(getUserCmd())
-	cliCmd.AddCommand(searchEntitlementsCmd())
-	cliCmd.AddCommand(tasksCmd())
-	cliCmd.AddCommand(loginCmd())
-	cliCmd.AddCommand(hasCmd())
-	cliCmd.AddCommand(tokenCmd())
-	cliCmd.AddCommand(terraformCmd())
-	cliCmd.AddCommand(decryptCredentialCmd())
 
 	err = cliCmd.ExecuteContext(ctx)
 	if err != nil {
